00177281c6e9e3659ab18c473f82d0e607c1fb4a
Viswajith MP <136588475+viswa2003@users.noreply.github.com>
2026-09-21 10:42:03 +0530

    Update Entry.java

--- OOP Java/Assignments/ArrayListExcercise/src/com/viswa/Entry.java
+++ OOP Java/Assignments/ArrayListExcercise/src/com/viswa/Entry.java
@@ -1,6 +1,9 @@
 package com.viswa;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
 
 public class Entry {
 
@@ -55,5 +58,58 @@ public class Entry {
 		} else {
 			System.out.println("6. " + color + " not found in the ArrayList");
 		}
+		
+		// 7
+		
+		Collections.sort(colors);
+		
+		System.out.println("After sorting : " + colors);
+		
+		//8
+		ArrayList<String> copyColors = new ArrayList<>(colors);
+		
+//		Collections.copy(copyColors, colors); //need to add empty strings using loop to allocate size
+		
+		System.out.println("After copying : " + copyColors);
+		
+		//9
+		Collections.shuffle(copyColors);
+		
+		System.out.println("After shuffling : " + copyColors);
+		
+		
+		//10
+		
+		Collections.reverse(copyColors);
+		
+		System.out.println("After reversing : " + copyColors);
+		
+		
+		//11
+		
+		TreeSet<String> treeSet = new TreeSet<>();
+		
+		treeSet.addAll(colors);
+		
+		System.out.println("tree set : " + treeSet);
+		
+		//12
+		TreeSet<String> secondTreeSet = new TreeSet<>(treeSet);
+		
+		System.out.println("second tree set : " + treeSet);
+		
+		//13
+		secondTreeSet.descendingSet();
+		
+		System.out.println("descending tree set : " + treeSet);
+		
+		//14
+		System.out.println("First : " + treeSet.first());
+		System.out.println("Last: " + treeSet.last());
+		
+		//15
+		String given = "Green";
+		
+		System.out.println(treeSet.ceiling(given));
 	}
 }
